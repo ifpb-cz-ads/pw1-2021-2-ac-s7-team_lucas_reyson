012fddd01f68e9c8ef7ec263d56e8c686b9ecc82
Reyson <73318309+nameNosyer@users.noreply.github.com>
2021-12-20 15:38:40 -0300

    Adicionando questão 04

--- 04.py
+++ 04.py
@@ -1,20 +1,7 @@
-parentese = input("Informe uma expressão com parênteses:")
+num_1 = float(input('Infrome a base do triângulo: '))
+num_2 = float(input('Infrome a altura do triângulo: '))
 
-pilha = []
-x = 0
+def area(num_1, num_2):
+    return (num_1 * num_2) / 2
 
-while x < len(parentese):
-    if parentese[x] == "(":
-        pilha.append("(")
-    if parentese[x] == ")":
-        if len(pilha) > 0:
-            topo = pilha.pop(-1)
-        else:
-            pilha.append(")")
-            break
-    x = x + 1
-
-if len(pilha) == 0:
-    print("OK")
-else:
-    print("Erro")
+print('Área do triangulo({}, {}) == {}'.format(num_1, num_2, area(num_1, num_2)))
