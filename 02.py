24515c47afd19772194b2a7747cefcb24d5e13ac
Reyson <73318309+nameNosyer@users.noreply.github.com>
2021-12-20 15:38:08 -0300

    Adicionando questão 02

--- 02.py
+++ 02.py
@@ -1,32 +1,7 @@
-lista_01 = []
-lista_02 = []
-lista_03 = []
+num_1 = int(input('Infrome o primeiro número: '))
+num_2 = int(input('Infrome o segundo número: '))
 
-while True:
-    nums = int(input("Infome um numero para a primeira lista (0 encerra):"))
-    if nums == 0:
-        break
-    lista_01.append(nums)
-while True:
-    nums = int(input("Infome um numero para a segunda lista (0 encerra):"))
-    if nums == 0:
-        break
-    lista_02.append(nums)
+def multiplo(num_1, num_2):
+    return num_1 % num_2 == 0
 
-juncao = lista_01[:]
-juncao.extend(lista_02)
-
-x = 0
-while x < len(juncao):
-    y = 0
-    while y < len(lista_03):
-        if juncao[x] == lista_03[y]:
-            break
-        y = y + 1
-    if y == len(lista_03):
-        lista_03.append(juncao[x])
-    x = x + 1
-x = 0
-while x < len(lista_03):
-    print("{}".format(lista_03[x]))
-    x = x + 1
+print('Múltiplo({},{}) == {}'.format(num_1, num_2, multiplo(num_1,num_2)))
